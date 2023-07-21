b12e5a2fa80893892bcd252183ed6e32acfc1ef7
unknown <panhareach.phat@student.cadt.edu.kh>
2023-07-21 23:27:45 +0700

    configuration layout

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    @vite('resources/css/app.css')
-</head>
-<body>
-    
+<x-layout>
+     
 <main class="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900">
     <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
         <article class="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
@@ -622,5 +613,4 @@
         </div>
     </div>
   </footer>
-</body>
-</html>
+</x-layout>
